9187e3a380dfbf70f3c0a721d8a632391cf293b4
Brandon <brandonsamuel72@gmail.com>
2019-04-26 18:59:13 -0700

    Initial commit. Created Server

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 
+app.get('/', (req, res) => {
+    console.log('Home');
+});
+
 app.listen(8080, (req, res) => {
     console.log('Server started on port 8080..');
 });
