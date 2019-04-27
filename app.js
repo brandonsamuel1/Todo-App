2df6798cf67912e365e72076285b186628de428e
Brandon <brandonsamuel72@gmail.com>
2019-04-26 18:32:33 -0700

    Initial commit. Created server

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -11,5 +12,5 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.listen(8080, (req, res) => {
-    console.log('Server started on port 8080...');
+    console.log('Server started on port 8080..');
 });
